0d750c2d4144e6f20a0c70790e87bacbd1cfb3a6
Joelisking <adujoel7@gmail.com>
2025-09-19 14:29:36 -0400

    responsive ui issues fixed

--- components/home/Shuttle.tsx
+++ components/home/Shuttle.tsx
@@ -6,12 +6,16 @@ import { Button, Icon } from '../ui';
 
 function Shuttle() {
   return (
-    <Container className="flex items-end bg-card-foreground rounded-2xl overflow-hidden mt-18 md:mt-24 lg:mt-28">
-      <div className="w-3/5">
-        <Image src={shuttle} alt="Shuttle Image" />
+    <Container className="flex flex-col lg:flex-row items-end bg-card-foreground md:rounded-2xl overflow-hidden mt-18 md:mt-24 lg:mt-28 !px-0">
+      <div className="w-full lg:w-3/5">
+        <Image
+          src={shuttle}
+          alt="Shuttle Image"
+          className="w-full h-auto"
+        />
       </div>
-      <div className="w-2/5 p-12">
-        <h3 className="text-6xl text-white font-bold">
+      <div className="w-full lg:w-2/5 p-6 sm:p-8 lg:p-12">
+        <h3 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl text-white font-bold">
           We Offer the best Shuttle services
           <span className="text-primary">.</span>
         </h3>
@@ -20,7 +24,7 @@ function Shuttle() {
           className="inline-flex hover:scale-105 transition-all duration-300">
           <Button
             variant={'primary'}
-            className="flex items-center mt-6">
+            className="flex items-center mt-4 sm:mt-6">
             Read More
             <Icon name="ArrowRight" />
           </Button>
